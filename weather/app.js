90bb3ebf52b815ffe89df7fb8d931e01463b0159
Ann Nagalah <annenagalah2020@gmail.com>
2026-03-18 11:59:41 +0000

    push to github

--- weather/app.js
+++ weather/app.js
@@ -6,6 +6,7 @@ let clouds = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressure = document.getElementById('pressure');
 let form = document.querySelector('form');
+let main = document.querySelector('main');
 form.addEventListener('submit',(event) => {
    event.preventDefault();
    if(valueSearch.value != ''){
@@ -24,5 +25,25 @@ const searchWeather = () => {
     city.querySelector('img').src='https://flagsapi.com/'+data.sys.country+'/shiny/32.png';
 
     temperature.querySelector('img').src ='http://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png';
-    temperature.querySelector('figcaption span')
-} 
+    temperature.querySelector('figcaption span').innerText = data.main.temp;
+    description.innerText = data.weather[0].description;
+    clouds.innerText = data.clouds.all;
+    humidity.innerText = data.main.humidity;
+    pressure.innerText = data.main.pressure;
+   }else{
+      // false
+      main.classList.add('error');
+      setTimeout(() => {
+         main.classList.remove('error');
+      }, 1000);
+
+   }
+   
+   valueSearch.value = '';
+})
+}
+const initApp = () => {
+   valueSearch.value = 'Ghana';
+   searchWeather();
+}
+initApp();
